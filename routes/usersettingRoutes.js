d6263c66aa6ec908d916e19c9df7ff0b22955571
rubiyadav <rubiflyeis@gmail.com>
2022-08-01 13:10:02 +0530

    server

--- routes/usersettingRoutes.js
+++ routes/usersettingRoutes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const express = require("express");
 
-const notification = require('../controllers/notificationController')
+const notification = require('../controllers/NotificationController')
 const { isAuthenticated } = require('../controllers/auth.controller')
 
-router.post('/add-notification',isAuthenticated,notification.UserSettings)
+router.post('/add-notification', isAuthenticated, notification.UserSettings)
 
 module.exports = router;
