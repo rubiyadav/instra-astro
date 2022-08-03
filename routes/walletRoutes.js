f641bef64e7099ef4f5844002f46cc969268a2be
rubiyadav <rubiflyeis@gmail.com>
2022-08-03 11:46:11 +0530

    new repo

--- routes/walletRoutes.js
+++ routes/walletRoutes.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const wallet = require('../controllers/WalletController')
+const { isAuthenticated } = require('../controllers/auth.controller')
 
-router.post('/wallet-data', wallet.walletPost)
-router.get('/get-by-find/:id', wallet.GetByFind)
-router.get('/get-by-id/:id', wallet.ViewWalletGetByID)
+router.post('/wallet-data', isAuthenticated , wallet.walletPost)
+router.get('/get-by-find/:id', isAuthenticated , wallet.GetByFind)
+router.get('/get-by-id/:id', isAuthenticated , wallet.ViewWalletGetByID)
 
 
 
