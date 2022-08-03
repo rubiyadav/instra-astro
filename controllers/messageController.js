8a2c9a92b461642284ff034d97e524262619a2dd
rubiyadav <rubiflyeis@gmail.com>
2022-08-03 19:11:59 +0530

    messagescontroller

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,11 +1,12 @@
 const Message = require('../models/Message');
 
+const moment = require("moment");
 // creating user
 module.exports.SaveMessageByUsers= async(req, res)=> {
   try {
     const {content, from, time, date,to} = req.body;
     if(!(content&& from&& time&& date&&to)) res.status(400).json({message:"Message not saved"})
-    const newMessage = await Message.create({content, from, time, date,to});
+    const newMessage = await Message.create({content, from, date,to, time: moment().format("llll"),});
 if (!newMessage) res.status(400).json({ message: "no message", status: false });
     res.status(200).json({
           message: "message is created successfully",
@@ -52,11 +53,10 @@ module.exports.getMessageByUsers= async(req, res)=> {
     const {to, from} = req.query;
     const message = await Message.find({ $or: [
         { $and: [{to}, {from}] },
-        { $and: [{to}, {from}] }
+        { $and: [{to:from}, {from:to}] }
     ]});
     res.status(200).json(message);
   } catch (e) {
       res.status(400).json(e.message)
   }
 }
-
--- models/message.js
+++ models/message.js
@@ -17,7 +17,8 @@ const mongoose = require('mongoose');
 const MessageSchema = new mongoose.Schema({
   content:{type:String,default:""},
   from:{type:String,default:""},
-  time:{type:String,default:""},
+    time: { type: String },
+//   time:{type:String,default:""},
   date:{type:String,default:""},
   to:{type:String,default:""}
 },{ timestamps: true})
