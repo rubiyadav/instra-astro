8a2a914049f1f3d3a6818033fad21fdd42089fc3
rubiyadav <rubiflyeis@gmail.com>
2022-08-04 12:11:56 +0530

    code

--- models/message.js
+++ models/message.js
@@ -10,6 +10,6 @@ const MessageSchema = new mongoose.Schema({
   to:{type:String,default:""}
 },{ timestamps: true})
 
-const Message = mongoose.model('message', MessageSchema);
+const Message = mongoose.model('Message', MessageSchema);
 
 module.exports = Message
